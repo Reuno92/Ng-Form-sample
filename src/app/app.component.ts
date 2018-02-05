c6edcc51734024034b9e22d64f5d1b5ad33898c1
Renaud Racinet <renaud.racinet@estiam.com>
2018-02-05 11:19:21 +0100

    Templpate form foundation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  message = 'This is the sample message';
 }
---
+++ src/app/template/template-form.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'template-form',
+    styleUrls: ['./template-form.component.css'],
+    templateUrl: './template-form.component.html'
+})
+
+export class TemplateFormComponent {}
