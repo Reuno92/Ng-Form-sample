7a4a6866a866874ada309b4860650949d36b1bf7
Renaud Racinet <renaud.racinet@estiam.com>
2018-02-07 14:38:55 +0100

    Form builder

--- src/app/reactive/reactive-form.component.ts
+++ src/app/reactive/reactive-form.component.ts
@@ -1,21 +1,29 @@
 import {Component, OnInit} from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
     selector: 'reactive-form',
     templateUrl: './reactive-form.component.html'
 })
+
 export class ReactiveFormComponent implements OnInit {
     form: FormGroup;
 
-    constructor() {}
+    constructor(private fb: FormBuilder) {}
 
     ngOnInit() {
 
         // Creation du formulaire.
+        /*
         this.form = new FormGroup({
               name: new FormControl(''),
           username: new FormControl('')
+        });
+        */
+
+        this.form = this.fb.group({
+           name:      [''],
+            username: ['']
         });
 
         console.log(this.form);
